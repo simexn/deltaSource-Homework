30cc11320f10c84f27847353a8316f28a6dd9e0d
simexn <sbuhalov3@gmail.com>
2022-07-06 11:07:55 +0300

    domashna

---
+++ src/countToHundred.java
@@ -0,0 +1,12 @@
+public class countToHundred {
+
+    public static void main(String[] args) {
+
+        for (int a = 1; ; a++) {
+            System.out.println(a);
+            if(a == 100){
+                break;
+            }
+        }
+    }
+}
